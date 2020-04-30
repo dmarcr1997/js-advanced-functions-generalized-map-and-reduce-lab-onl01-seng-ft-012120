72f6e266401278185ccdd1785c593a8fbb9325d2
Damon Marc Rocha <dmarcr1997@gmail.com>
2020-04-30 16:21:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(src, f){
+    let newA = [];
+    for(let i = 0; i < src.length; i++){
+        newA.push(f(src[i]));
+    }
+    return newA;
+}
+
+function reduce(src, f, start){
+    let s;
+    let i;
+    if (!!start){
+        s = start;
+        i = 0;
+    }
+    else{
+        i = 1;
+        s = src[0];
+    }
+    for(; i < src.length; i++){
+        s = f(src[i], s);            
+    }
+    return s;
+
+}
